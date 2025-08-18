f1d9c64ef9bdc431841859dd65bb3dcbe2b6cff9
Gadi Cohen <dragon@wastelands.net>
2025-08-18 14:42:19 +0100

    fix(quoteCombine): restore quoteCombine functionality + tests

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import createYahooFinance from "./createYahooFinance.ts";
 import modules from "./modules/index.ts";
+import quoteCombine from "./other/quoteCombine.ts";
 
-const YahooFinance = createYahooFinance({ modules });
+const YahooFinance = createYahooFinance({
+  modules: { ...modules, quoteCombine },
+});
 export default YahooFinance;
--- src/other/quoteCombine.ts
+++ src/other/quoteCombine.ts
@@ -3,12 +3,22 @@ import type {
   ModuleOptionsWithValidateFalse,
   ModuleOptionsWithValidateTrue,
   ModuleThis,
-} from "../lib/moduleCommon.js";
+} from "../lib/moduleCommon.ts";
 
-import type { Quote, QuoteOptions } from "../modules/quote.js";
-import quote from "../modules/quote.js";
+import { getTypedDefinitions } from "../lib/validate/index.ts";
 
-import validateAndCoerceTypes from "../lib/validateAndCoerceTypes.js";
+import type {
+  Quote,
+  QuoteOptions,
+  QuoteOptionsWithReturnArray,
+  QuoteResponseArray,
+} from "../modules/quote.ts";
+import quote from "../modules/quote.ts";
+
+import validateAndCoerceTypes from "../lib/validateAndCoerceTypes.ts";
+
+import schema from "../modules/quote.schema.json" with { type: "json" };
+const definitions = getTypedDefinitions(schema);
 
 const DEBOUNCE_TIME = 50;
 
@@ -26,14 +36,14 @@ export default function quoteCombine(
   query: string,
   queryOptionsOverrides?: QuoteOptions,
   moduleOptions?: ModuleOptionsWithValidateFalse,
-): Promise<any>;
+): Promise<unknown>;
 
 export default function quoteCombine(
   this: ModuleThis,
   query: string,
-  queryOptionsOverrides: QuoteOptions = {},
+  _queryOptionsOverrides: QuoteOptions = {},
   moduleOptions?: ModuleOptions,
-): Promise<any> {
+): Promise<unknown> {
   const symbol = query;
 
   if (typeof symbol !== "string") {
@@ -43,13 +53,32 @@ export default function quoteCombine(
     );
   }
 
-  validateAndCoerceTypes({
-    source: "quoteCombine",
-    type: "options",
-    object: queryOptionsOverrides,
-    schemaKey: "#/definitions/QuoteOptions",
-    options: this._opts.validation,
-  });
+  if (
+    _queryOptionsOverrides.return && _queryOptionsOverrides.return !== "array"
+  ) {
+    throw new Error("Can't specify other return types using quoteCombine()");
+  }
+  const queryOptionsOverrides =
+    _queryOptionsOverrides as QuoteOptionsWithReturnArray;
+
+  const validateOptions = !moduleOptions ||
+    moduleOptions.validateOptions === undefined ||
+    moduleOptions.validateOptions === true;
+
+  try {
+    validateAndCoerceTypes({
+      source: "quoteCombine",
+      type: "options",
+      object: queryOptionsOverrides,
+      definitions,
+      schemaKey: "#/definitions/QuoteOptions",
+      options: this._opts.validation,
+      logger: this._opts.logger,
+      logObj: this._logObj,
+    });
+  } catch (error) {
+    if (validateOptions) throw error;
+  }
 
   // Make sure we only combine requests with same options
   const slug = JSON.stringify(queryOptionsOverrides);
@@ -66,8 +95,6 @@ export default function quoteCombine(
 
   if (entry.timeout) clearTimeout(entry.timeout);
 
-  const thisQuote = quote.bind(this);
-
   return new Promise((resolve, reject) => {
     let symbolPromiseCallbacks = entry.symbols.get(symbol);
     /* istanbul ignore else */
@@ -82,31 +109,38 @@ export default function quoteCombine(
       slugMap.delete(slug);
 
       const symbols: string[] = Array.from(entry.symbols.keys());
-      // @ts-ignore
-      thisQuote(symbols, queryOptionsOverrides, moduleOptions)
-        .then((results) => {
-          for (let result of results) {
-            for (let promise of entry.symbols.get(result.symbol)) {
-              promise.resolve(result);
-              promise.resolved = true;
-            }
-          }
 
-          // Check for symbols we asked for and didn't get back,
-          // e.g. non-existant symbols (#150)
-          for (let [symbol, promises] of entry.symbols) {
-            for (let promise of promises) {
-              if (!promise.resolved) {
-                promise.resolve(undefined);
-              }
+      const thisQuote = quote.bind(this) as (
+        symbols: string[],
+        options?: QuoteOptionsWithReturnArray,
+        moduleOptions?: ModuleOptionsWithValidateTrue,
+      ) => Promise<QuoteResponseArray>;
+
+      thisQuote(symbols, queryOptionsOverrides, {
+        ...moduleOptions,
+        validateResult: true,
+      }).then((results) => {
+        for (const result of results) {
+          for (const promise of entry.symbols.get(result.symbol)) {
+            promise.resolve(result);
+            promise.resolved = true;
+          }
+        }
+
+        // Check for symbols we asked for and didn't get back,
+        // e.g. non-existant symbols (#150)
+        for (const [_symbol, promises] of entry.symbols) {
+          for (const promise of promises) {
+            if (!promise.resolved) {
+              promise.resolve(undefined);
             }
           }
-        })
-        .catch((error) => {
-          for (let symbolPromiseCallbacks of entry.symbols.values()) {
-            for (let promise of symbolPromiseCallbacks) promise.reject(error);
-          }
-        });
+        }
+      }).catch((error) => {
+        for (const symbolPromiseCallbacks of entry.symbols.values()) {
+          for (const promise of symbolPromiseCallbacks) promise.reject(error);
+        }
+      });
     }, DEBOUNCE_TIME);
   });
 }
